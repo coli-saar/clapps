de1f751bcbfa1e73fecba7d327dfcb07028ecaba
Alexander Koller <koller@ling.uni-potsdam.de>
2016-07-31 18:26:23 +0200

    oops

---
+++ user.py
@@ -0,0 +1,28 @@
+from flask_login import UserMixin
+
+
+class User():
+    def __init__(self, id, name, passwd):
+        self.id = id
+        self.name = name
+        self.passwd = passwd
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.id
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return True
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
+
+    def __str__(self):
+        return "%s: %s (pw %s)" % (self.id, self.name, self.passwd)
